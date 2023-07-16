7b9af7b18bd9ace4c439ec2c4aece7ba6d97c6aa
Kaivalya Mendki <kaivalya.mendki@nutanix.com>
2023-07-16 11:02:20 +0530

    Create new go package

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thephenom1708/go-pkg
+
+go 1.20
+
+require github.com/gofrs/uuid v4.4.0+incompatible
---
+++ main.go
@@ -0,0 +1,12 @@
+package go-pkg
+
+import (
+	"fmt"
+
+	_ "github.com/gofrs/uuid"
+)
+
+func X() {
+	fmt.Println("Hello World!")
+}
+
